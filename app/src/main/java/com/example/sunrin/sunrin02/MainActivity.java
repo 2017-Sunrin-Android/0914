71b1ff16811c43435e926b8f54798fd56f16d1a6
LEE EUNSEO <eunseo9808@naver.com>
2017-09-15 16:45:52 +0900

    Sign Up

--- app/src/main/java/com/example/sunrin/sunrin02/MainActivity.java
+++ app/src/main/java/com/example/sunrin/sunrin02/MainActivity.java
@@ -8,8 +8,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    User []user=new User[200];
-    int user_num=0;
+    static User []user=new User[200];
+    static int user_num=0;
     EditText username;
     EditText password;
     @Override
@@ -22,21 +22,28 @@ public class MainActivity extends AppCompatActivity {
         username=(EditText)findViewById(R.id.username);
         password=(EditText)findViewById(R.id.password);
     }
-    void onLoginClicked(View v){
-
+    public void onLoginClicked(View v){
         for(int i=0; i<user_num; i++){
             if(user[i].id.equals(username.getText().toString()) &&
                     user[i].password.equals(password.getText().toString())){
                 Intent intent = new Intent(MainActivity.this, LoginedActivity.class);
                 startActivity(intent);
-            }
-            else{
-                Toast.makeText(getApplicationContext(), "Check your ID and password", Toast.LENGTH_SHORT).show();
+                return ;
             }
         }
+        Toast.makeText(getApplicationContext(), "Check your ID and Password", Toast.LENGTH_SHORT).show();
     }
-    void onSignupClicked(View v){
+    public void onSignupClicked(View v){
         Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent,0);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode==0){
+            String username_str=data.getStringExtra("USERNAME");
+            username.setText(username_str);
+        }
+    }
+
 }
--- app/src/main/java/com/example/sunrin/sunrin02/SignupActivity.java
+++ app/src/main/java/com/example/sunrin/sunrin02/SignupActivity.java
@@ -1,15 +1,42 @@
 package com.example.sunrin.sunrin02;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Sunrin on 2017-09-14.
  */
 
 public class SignupActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    EditText passwordCheck;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        passwordCheck=(EditText)findViewById(R.id.passwordCheck);
+    }
+
+    public void signupClick(View v){
+        String username_str=username.getText().toString();
+        String password_str=password.getText().toString();
+        String passwordCheck_str=passwordCheck.getText().toString();
+        if(password_str.equals(passwordCheck_str)){
+            User user = new User(username_str,password_str);
+            MainActivity.user[MainActivity.user_num++]=user;
+            Intent intent =new Intent();
+            intent.putExtra("USERNAME", username_str);
+            setResult(200,intent);
+            finish();
+        }else{
+            Toast.makeText(getApplicationContext(),"Check your password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
